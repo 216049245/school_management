f8c7debece378a1e04b64112b0ad66e4ddc98aec
Monehi Tuoane <monehituoane@gmail.com>
2022-06-12 20:49:13 +0200

    Created StudentAddress.java entity

---
+++ src/main/java/domain/StudentAddress.java
@@ -0,0 +1,17 @@
+package domain;
+
+
+public class StudentAddress {
+
+    private Student studentId;
+    private  String address;
+
+    /*
+     * private Address address;
+     * Need Lutho to finish the Address.java entity
+     * in order to inherit
+     * the "Address" attributes
+     */
+
+
+}
