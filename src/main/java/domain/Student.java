f81092d34b8e5dec624bbbf23bf13263b0310e6e
Monehi Tuoane <monehituoane@gmail.com>
2022-06-12 20:12:50 +0200

    Added Builder class for Student.java entity

--- src/main/java/domain/Student.java
+++ src/main/java/domain/Student.java
@@ -2,8 +2,63 @@ package domain;
 
 public class Student {
 
-    private String studentId;
-    private String email;
-    private String name;
+    private final String
+            studentId,
+            email,
+            name;
 
+    //Builder Class
+    private Student(Builder builder) {
+
+        this.studentId = builder.studentId;
+        this.email = builder.email;
+        this.name = builder.name;
+
+    }
+
+    //Getters
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    //Builder static Class
+    public static class Builder {
+
+        private String
+                studentId,
+                email,
+                name;
+
+        public Builder studentId(String studentId) {
+            this.studentId = studentId;
+            return this;
+        }
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId='" + studentId + '\'' +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
