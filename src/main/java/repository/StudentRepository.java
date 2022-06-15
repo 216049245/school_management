744c5f25a8b772c1868ca6ff2a41460016146779
Monehi Tuoane <monehituoane@gmail.com>
2022-06-15 16:27:46 +0200

    Completed CRUD for StudentRepository

--- src/main/java/repository/StudentRepository.java
+++ src/main/java/repository/StudentRepository.java
@@ -8,33 +8,59 @@ package repository;
 
 import domain.Student;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 //CRUD
 public class StudentRepository {
 
+    //Implement singleton
+    private final List<Student> studentList;
+    private static StudentRepository STUDENT_REPO;
+
+    private StudentRepository () {
+        this.studentList = new ArrayList<>();
+    }
+
+    public static StudentRepository studentRepository() {
+        if (STUDENT_REPO == null)
+            STUDENT_REPO = new StudentRepository();
+        return STUDENT_REPO;
+    }
+
+
     public Student create(Student student) {
-        //save and return if successful
+        this.studentList.add(student);
         return  student;
     }
 
-    public Student read(String studentId) {
-        //find student that matches the id and return
-        return null;
+    //"Optional <method>..." prevents a return of null
+    public Optional<Student> read(String studentId) {
+        //find student that matches the student id and return
+        return this.studentList.stream().filter(s -> s.getStudentId().equalsIgnoreCase(studentId))
+                .findAny();
+
     }
 
     public Student  update(Student student) {
         //check if student exists, if yes update and return if successful
+        Optional<Student> read = read(student.getStudentId());
+        if (read.isPresent()) {
+            delete(String.valueOf(read.get()));
+            create(student);
+        }
         return student;
     }
 
     public void delete(String studentId) {
         //finds the student if required and delete
+        this.studentList.remove(studentId);
     }
 
     public List<Student> findAll() {
         //returns all the stored students
-        return null;
+        return this.studentList;
     }
 
 
