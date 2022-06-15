d9df3537a05c1839ab6ccc33a2fd20d21e29682f
Monehi Tuoane <monehituoane@gmail.com>
2022-06-15 14:58:39 +0200

    Initialised CRUD for StudentRepository

--- src/main/java/repository/StudentRepository.java
+++ src/main/java/repository/StudentRepository.java
@@ -1,6 +1,14 @@
+/*
+StudentRepository.java
+Author: Monehi Tuoane (219350744)
+Date: 15 June 2022
+*/
+
 package repository;
 
 import domain.Student;
+
+import java.util.List;
 
 //CRUD
 public class StudentRepository {
@@ -8,6 +16,25 @@ public class StudentRepository {
     public Student create(Student student) {
         //save and return if successful
         return  student;
+    }
+
+    public Student read(String studentId) {
+        //find student that matches the id and return
+        return null;
+    }
+
+    public Student  update(Student student) {
+        //check if student exists, if yes update and return if successful
+        return student;
+    }
+
+    public void delete(String studentId) {
+        //finds the student if required and delete
+    }
+
+    public List<Student> findAll() {
+        //returns all the stored students
+        return null;
     }
 
 
