5b2c1a80c4300e2fcd3d290aeb766a038559b564
Monehi Tuoane <monehituoane@gmail.com>
2022-06-15 23:02:44 +0200

    Modification in StudentFactory to ensure successful tests in StudentRepositoryTest

--- src/main/java/factory/StudentFactory.java
+++ src/main/java/factory/StudentFactory.java
@@ -12,4 +12,15 @@ public class StudentFactory {
                 .build();
         return Student;
     }
+
+//Need to sort out the build in the test case
+    public static Student build(String s, String s1) {
+        return  build(s, s1);
+    }
+
+    /*
+    public static Student build(String s, String s1) {
+        return build("test-id", "test-student");
+    }
+     */
 }
