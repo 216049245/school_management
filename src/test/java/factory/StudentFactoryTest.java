f6c875d70685ce18d0dc25f471673c2a5f7803a6
Monehi Tuoane <monehituoane@gmail.com>
2022-06-17 17:18:06 +0200

    StudentFactoryTest: tests for successful test and unsuccessful test. All tests Passed!

--- src/test/java/factory/StudentFactoryTest.java
+++ src/test/java/factory/StudentFactoryTest.java
@@ -1,16 +1,36 @@
+/*
+StudentFactoryTest.java
+Author: Monehi Tuoane (219350744)
+Date: 17 June 2022
+*/
+
 package factory;
 
 import domain.Student;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class StudentFactoryTest {
 
     @Test
-    public void build() {
-        Student student = StudentFactory.build("", "", "");
+    public void buildWithoutError() {
+        Student student = StudentFactory.build("123456789", "student@mail", "Mr Stud");
         System.out.println(student);
         assertNotNull(student);
+
+    }
+
+    @Test
+    public void buildWithError() {
+
+        Exception exception =  assertThrows(IllegalArgumentException.class, () ->
+                StudentFactory.build(null, "student@mail", "Mr Stud"));
+
+        String exceptionMessage = exception.getMessage();
+        System.out.println(exceptionMessage);
+        assertSame("Student ID is required!", exceptionMessage);
+
 
     }
 
