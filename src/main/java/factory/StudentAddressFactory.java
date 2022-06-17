16363d2e18e808f033300fea2568442891629c36
Monehi Tuoane <monehituoane@gmail.com>
2022-06-17 17:16:40 +0200

    StudentAddressFactoryTest: tests for successful test and unsuccessful test. All tests Passed!

--- src/main/java/factory/StudentAddressFactory.java
+++ src/main/java/factory/StudentAddressFactory.java
@@ -1,3 +1,9 @@
+/*
+StudentAddressFactory.java
+Author: Monehi Tuoane (219350744)
+Date: 17 June 2022
+*/
+
 package factory;
 
 import domain.StudentAddress;
@@ -6,6 +12,15 @@ public class StudentAddressFactory {
 
 
     public static StudentAddress build(String studentId, String address) {
+
+        //Inform caller of the missing studentId
+        if (studentId == null || studentId.isEmpty())
+            throw new IllegalArgumentException("Student ID is required!");
+
+        //Inform caller of the missing email
+        if (address == null || address.isEmpty())
+            throw new IllegalArgumentException("Student address is required!");
+
 
         return new StudentAddress.Builder().studentId(studentId)
                 .address(address)
